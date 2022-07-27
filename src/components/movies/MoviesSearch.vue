d9f595165a271b2d0ae6fc0632ce053a89cc4566
ThiagoFTelles <atendimento.telles@gmail.com>
2022-07-27 19:12:02 -0300

    estiliza o campo de busca e os inputs do site

--- src/components/movies/MoviesSearch.vue
+++ src/components/movies/MoviesSearch.vue
@@ -5,10 +5,11 @@
       id="search"
       type="text"
       v-model="search"
+      placeholder="Buscar"
     >
     <input
       type="submit"
-      id="lupa"
+      id="submit"
       value="Buscar"
       @click.prevent="searchMovies"
     >
@@ -31,5 +32,32 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+  padding: 0 10px;
+}
+#search {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+#search:hover,
+#search:focus {
+  transform: scale(1.1);
+}
+#submit {
+  background: url("../../assets/icons/search.svg") no-repeat center center;
+  border: none;
+  box-shadow: none;
+  cursor: pointer;
+  height: 62px;
+  position: absolute;
+  width: 60px;
+  right: 0px;
+  text-indent: -150px;
+  top: 0px;
+}
 </style>

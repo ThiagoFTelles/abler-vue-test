bcef7e7f9be527aa9eab96eb0cc6a24810f94040
ThiagoFTelles <atendimento.telles@gmail.com>
2022-07-27 17:38:57 -0300

    adiciona a primeira requisição da API na lista de filmes

--- src/components/movies/MoviesList.vue
+++ src/components/movies/MoviesList.vue
@@ -1,12 +1,42 @@
 <template>
-  <div>
-    <p>Lista:</p>
-  </div>
+  <section class="movies-container">
+    <div
+      v-for="movie in movies"
+      :key="movie.id"
+    >
+      <img
+        :src="`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.backdrop_path}`"
+        :alt="movie.original_title"
+      >
+      <h2 class="titulo">
+        {{movie.original_title}}
+      </h2>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "MoviesList",
+  data() {
+    return {
+      movies: null,
+    };
+  },
+  methods: {
+    getMovies() {
+      fetch(
+        "https://api.themoviedb.org/3/movie/popular?api_key=9528e187a9d83ace76fff9ee13f5e837&language=en-US&page=1"
+      )
+        .then((r) => r.json())
+        .then((r) => {
+          this.movies = r.results;
+        });
+    },
+  },
+  created() {
+    this.getMovies();
+  },
 };
 </script>
 
